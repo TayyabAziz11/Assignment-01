616d0ad7913e419a70890dcb033dc02ea7aad43f
TayyabAziz11 <159780590+TayyabAziz11@users.noreply.github.com>
2024-03-12 15:31:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+//  Assignment No = 01
+var personName = "Tayyab";
+// Print a Message to the person
+console.log("".concat(personName), " This is my first assignment ");
---
+++ main.ts
@@ -0,0 +1,7 @@
+//  Assignment No = 01
+
+let personName: string= "Tayyab"
+
+// Print a Message to the person
+
+console.log(`${personName}`, " This is my first assignment ");
